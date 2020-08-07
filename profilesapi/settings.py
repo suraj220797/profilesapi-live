4795581b068ed9fa0d550283a22f1b3f1231c074
Suraj <sonawanesuraj74@gmail.com>
2020-08-07 14:43:33 +0530

    my third commmit

--- profilesapi/settings.py
+++ profilesapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'surajcf9s2=)^wv_zfblb*61qj^gq)h-q+1rywj1q34$m3e+&w-4xgy'
+SECRET_KEY = 'cf9s2=)^wv_zfblb*61qj^gq)h-q+1rywj1q34$m3e+&w-4xgy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
